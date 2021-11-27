10eb2ee0240e1e6815336a2da39a4eeeb6b8d2ba
32494 <amine.nemmaoui@student.vinci.be>
2021-11-27 15:20:21 +0100

    test calcul minutes

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,4 +3,16 @@
 class BerlinClock
 {
 
+    public function calculateMinutes($timer)
+    {
+        $var = explode(":", $timer);
+        $minutes = strval($var[0]);
+        $mod = $minutes % 5;
+        $string = "";
+        for ($i = 0; $i < $mod; $i++) {
+            $string .= "Y";
+        }
+        return $string;
+    }
+
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,9 +1,17 @@
 <?php
-
-
+require "vendor/autoload.php";
+require "BerlinClock.php";
 use PHPUnit\Framework\TestCase;
 
 class BerlinClockTest extends TestCase
 {
+    //TEST SIMPLES MINUTES
+
+    public function test_CalculateMinutes_given12H00_returnNothing(){
+        $test = new BerlinClock();
+        $timer="12:00:00";
+        $actual = $test->calculateMinutes($timer);
+        $this->assertEquals("",$actual);
+    }
 
 }
